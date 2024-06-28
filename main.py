a1556680a079e623bed03803e687c9cb75e3d52b
Vũ Quang Đại (Bevis Vũ) <127699283+VQD-BSV@users.noreply.github.com>
2024-06-28 16:13:57 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-def sreen():
-   if platform.system() == "Windows":
-       os.system("cls")
+# Print header
+def header():
+   os.system("cls")
 
-   with open("sreen.txt", "r") as r:
+   with open("header.txt", "r") as r:
       print(r.read())
 
+# Print partition info
 def get_disk_info():
    # get disk info
    drive_info = get_drive_info()
@@ -21,64 +22,66 @@ def get_disk_info():
    # out ket qua
    return info_part
 
-def tool(part):
-   print("""\n#Scan #Get_Image""")
-
+# Tool
+def check(partition):
    key = input("\n==> ")
 
-   # Get key & save image
    if key == "Get_Image":
-      disk = r'\\.\{}:'.format(part) # read disk
-      out_file = f"Part_{part}.img"  # out file
+      get_image(partition)
 
-      # read & write
-      with open(disk, 'rb') as disk:
-          with open(out_file, 'wb') as w:
-              while True:
-                  byte = disk.read(1048576) #1 MB
-                  if not byte:
-                      break  # Exit loop
-                  # Write byte
-                  w.write(byte)
-                  
-#============main===========
-from Disk.get_disk import get_drive_info
-from Scan.scan_image import scan_image
+# Get Image      
+def get_image(partition):
+   disk = r'\\.\{}:'.format(partition) # read disk
+   out_file = f"Partition_{partition}.img"  # out file
+
+   # read & write
+   with open(disk, 'rb') as disk:
+       with open(out_file, 'wb') as w:
+           while True:
+               byte = disk.read(1048576) # 1 MB
+               if not byte:
+                   break  # Exit loop
+               # Write byte
+               w.write(byte)
+
+# Scan Image
+def scan_image():
+   # input & print
+   print("Enter File Path"); image = input("\n==> "); header()
+   print("=> ",image)
+   # input & print
+   print("CR2/?"); file = input("\n==> "); header()
+   print("=> ",image); print("=> ",file)
+
+   if image != "x":      
+      if file == "CR2":
+         print("\n")
+         scan_cr2(image)
+
+#=============================================
+from Tool.get_disk import get_drive_info
+from Tool.scan_cr2 import scan_cr2
 import platform, os, time
 
-
+# Loop
 while True:
-   # Run form 
-   sreen()
-   info_part = get_disk_info()
+   # Create Form
+   header(); info_part = get_disk_info()
    print("\n#Scan_Image")
-#===================================
-   #Select Part
-   sel_part = input("\n==> ")
 
-   # Exit
-   if sel_part == "x":
-      break
-#===================================
-   #Scan_Image
-   if sel_part == "Scan_Image":
-      sreen()
-      image = input("Enter File Path: ")
+   # Choose partition
+   select_partition = input("\n==> ")
+   # break 
+   if select_partition == "x": break
+   # Scan_Image
+   if select_partition == "Scan_Image":
+      header()
+      scan_image()
+
+   # if int   
+   elif int(select_partition) != 0:
+      header()
+      # get partition & print
+      get_part = info_part.splitlines()[int(select_partition) - 1]
+      print(get_part[4:]); print("""\n#Scan #Get_Image"""); check(get_part[7])
       
-      sreen()
-      print(f"=> {image}")
-      scan_image(image)
-   #===================================
-   # Check Select Part
-   elif int(sel_part) != 0:
-      sreen()
-
-      # get line
-      info_part = info_part.splitlines()
-      info_part = info_part[int(sel_part) - 1]
-
-      # key 4 & key 7
-      print(info_part[4:])
-      tool(info_part[7])
-
-
